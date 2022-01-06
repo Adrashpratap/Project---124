9c913a7efff71e7fea59dc74173021c7431791b8
Adrashpratap <77541467+Adrashpratap@users.noreply.github.com>
2022-01-06 19:40:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 song1 = "";
 song2 = "";
+
 leftwristX = "";
 leftwristY = "";
+
 rightwristX  = "";
 rightwristY = "";
+
+songplay = "";
+
+score_leftwrist = "";
 
 function setup(){
     canvas = createCanvas(600,500);
@@ -24,6 +30,9 @@ function modelLoaded()
 function gotPoses(results){
   if (results.length > 0) {
       console.log(results);
+       
+      score_leftwrist = results[0].pose.keypoints[9].score;
+      console.log("Score_leftwrist = "+ score_leftwrist);
 
       leftwristX = results[0].pose.leftWrist.x;
       leftwristY = results[0].pose.leftWrist.y;
@@ -43,5 +52,22 @@ function preload(){
 
 function draw(){
     image(video,0,0,600,500);
+    songplay = song1.isPlaying();
+    console.log("Song status = "+ songplay);
+    fill("FF000");
+    stroke("FF000")
+   
+    if (score_leftwrist > 0.2) {
+        circle(leftwristX,leftwristY,20);
+        song2.stop();
+
+     if (songplay = false) {
+            song1.play();
+            document.getElementById("play").innerHTML = "song1";
+        }
+    }
 }
 
+function playthesong(){
+    song1.play();
+}
